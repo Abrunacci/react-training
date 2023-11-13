686e9c959ebc9a270ca5e4228bd712795bdedc88
Alejandro Brunacci <abrunacci87@gmail.com>
2023-11-13 12:21:07 -0300

    Todo and Task components creation

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,10 +1,20 @@
 import {useState} from "react";
+import Task from "./Task.jsx";
 
 export default function Todo(){
+
     const [todo, setTodo] = useState("")
+    const [tasks, setTasks] = useState([])
+
+    function handleSubmit(e){
+        e.preventDefault();
+        setTasks([...tasks, todo]);
+        setTodo("")
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     value={todo}
                     onChange={(e)=>setTodo(e.target.value)}
@@ -13,5 +23,8 @@ export default function Todo(){
                 <button type="submit">Add</button>
             </form>
 
+            {tasks.map((task) => (
+                <Task key={task} name={task}/>
+            ))}
         </div>)
 }
